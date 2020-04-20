4d3a0453213195880ffc17442657524ce66331a1
Junki Tomatsu <me@junki-t.net>
2020-04-20 11:41:15 +0900

    CORS用ヘッダー出力の修正

--- utils/cors.php
+++ utils/cors.php
@@ -1,6 +1,10 @@
 <?php
+    function endsWith($haystack, $needle) {
+        return (strlen($haystack) > strlen($needle)) ? (substr($haystack, -strlen($needle)) == $needle) : false;
+    }
+
     $accept_anywhere = false;  //アクセス元に関わらず許可する場合はこれをtrueにする
-    // ↑がfalseの場合は↓のhostのみにCORS許可を与える(ワイルドカード不可)
+    // ↑がfalseの場合は↓のhostのみにCORS許可を与える(後方一致のみ可能(*.example.com))
     $accept_origin = array(
         "test-gis-dev.junki-t.net",
         "localhost",
@@ -10,6 +14,18 @@
         if($accept_anywhere || in_array(str_replace(array("http://", "https://"), "", $_SERVER["HTTP_ORIGIN"]), $accept_origin)) {
             header("Access-Control-Allow-Origin: " . $_SERVER["HTTP_ORIGIN"]);
             header("Access-Control-Allow-Credentials: true");
+        } else {
+            foreach($accept_origin as $orig){
+                if(strpos($orig,'*') !== false) {
+                    $orig = str_replace("*", "", $orig);
+
+                    if(endsWith($_SERVER["HTTP_ORIGIN"], $orig)){
+                        header("Access-Control-Allow-Origin: " . $_SERVER["HTTP_ORIGIN"]);
+                        header("Access-Control-Allow-Credentials: true");
+                        break;
+                    }
+                }
+            }
         }
     }
 ?>
